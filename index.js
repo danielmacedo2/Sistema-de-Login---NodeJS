dc127d1bd755e6b60286ac8d8787fed1d39be910
Daniel Macedo <danielmsv2011@hotmail.com>
2022-06-12 08:40:38 -0300

    Setup do projeto

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const session = require('express-session');
+
+const port = 4000;
+var path = require('path');
+const app = express();
+
+
+
+app.engine('html', require('ejs'));
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.listen(port, () => {
+    console.log('Servidor rodando na porta 4000');
+})
